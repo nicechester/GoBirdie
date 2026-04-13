253e96799eeca04492472b6b490b23501263d049
Chester Kim <violkim@gmail.com>
2026-04-13 09:15:04 -0700

    Lock orientation to portrait during active round
    Uses AppDelegate to control supportedInterfaceOrientations.
    Locks to portrait when activeRound is non-nil, unlocks on end.

--- GoBirdie/GoBirdieApp.swift
+++ GoBirdie/GoBirdieApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock: UIInterfaceOrientationMask = .all
+
+    func application(_ application: UIApplication,
+                     supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        Self.orientationLock
+    }
+}
 
 @main
 struct GoBirdieApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @StateObject private var appState = AppState()
     @Environment(\.scenePhase) private var scenePhase
 
@@ -24,6 +35,9 @@ struct GoBirdieApp: App {
                     if phase == .background || phase == .inactive {
                         appState.saveInProgress()
                     }
+                }
+                .onReceive(appState.$activeRound) { round in
+                    AppDelegate.orientationLock = round != nil ? .portrait : .all
                 }
         }
     }
